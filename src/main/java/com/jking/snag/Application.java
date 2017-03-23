d1f4d2bbe22cb3fa7f0bad55e664c0f720a0e77a
john king <jking13@vt.edu>
2017-03-23 18:24:02 -0400

    application and swagger config

---
+++ src/main/java/com/jking/snag/Application.java
@@ -0,0 +1,30 @@
+
+package com.jking.snag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper() {
+    	ObjectMapper mapper = new ObjectMapper();
+    	mapper.setDateFormat(new ISO8601DateFormat());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        return mapper;
+    }
+
+}
