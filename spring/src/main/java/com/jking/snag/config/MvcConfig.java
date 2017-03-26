2221ac7abe7dc20d17fa2cd181be65925362e1d4
john king <jking13@vt.edu>
2017-03-26 16:55:44 -0400

    specifying allowed cors methods

--- spring/src/main/java/com/jking/snag/config/MvcConfig.java
+++ spring/src/main/java/com/jking/snag/config/MvcConfig.java
@@ -12,6 +12,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
